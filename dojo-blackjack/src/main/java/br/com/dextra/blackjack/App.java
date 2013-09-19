2d21add6220d76d472d370843bb35e1e2acbdfbe
thiago-oliveira <thiago.op93@gmail.com>
2013-09-19 17:44:43 -0300

    Ajustes

--- dojo-blackjack/src/main/java/br/com/dextra/blackjack/App.java
+++ dojo-blackjack/src/main/java/br/com/dextra/blackjack/App.java
@@ -1,13 +1,7 @@
 package br.com.dextra.blackjack;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
